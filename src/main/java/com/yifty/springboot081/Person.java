0110489933a73d2aa848a0472dea931d33b27863
Yifty <yifty08@icloud.com>
2018-07-16 20:27:50 -0400

    Initial commit

---
+++ src/main/java/com/yifty/springboot081/Person.java
@@ -0,0 +1,39 @@
+package com.yifty.springboot081;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/com/yifty/springboot081/Springboot081Application.java
@@ -0,0 +1,12 @@
+package com.yifty.springboot081;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot081Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot081Application.class, args);
+    }
+}
